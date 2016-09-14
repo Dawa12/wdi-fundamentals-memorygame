ae97f138c3513bccf8217fd500b743a2f67d39d9
Dawa <dasherpa7@gmail.com>
2016-09-14 10:49:54 -0400

    updated JS to manipulate DOM; added 4 more cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,41 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-if (cardOne === cardTwo) {
-  window.alert("You found a match!");
-} else if (cardThree === cardFour) {
-  alert("You found a match!")
-} else {
-  alert ("Sorry, try again.")
+// if (cardOne === cardTwo) {
+//   window.alert("You found a match!");
+// } else if (cardThree === cardFour) {
+//   alert("You found a match!")
+// } else {
+//   alert ("Sorry, try again.")
+// }
+
+var createBoard = function()
+{
+  var gameBoard = document.getElementById("game-board");
+
+  for (var i=0; i < 4; i++) {
+    var newCard = document.createElement("div");
+    newCard.className = "card";
+    gameBoard.appendChild(newCard);
+  }
 }
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
