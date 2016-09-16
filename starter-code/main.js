173496471f9339f9baa3d9b5e923eae157e3bd86
Dawa <dasherpa7@gmail.com>
2016-09-16 17:22:37 -0400

    completed game, updated to match cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,41 +1,60 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var gameBoard = document.getElementById("game-board");
 
-// if (cardOne === cardTwo) {
-//   window.alert("You found a match!");
-// } else if (cardThree === cardFour) {
-//   alert("You found a match!")
-// } else {
-//   alert ("Sorry, try again.")
-// }
-
-var createBoard = function()
+var createBoard = function() 
 {
-  var gameBoard = document.getElementById("game-board");
-
-  for (var i=0; i < 4; i++) {
+  for (var i=0; i < cards.length; i++)
+  {
     var newCard = document.createElement("div");
     newCard.className = "card";
+    newCard.setAttribute("data-card", cards[i]);
+    newCard.addEventListener("click", isTwoCards);
+    
+    gameBoard.appendChild(newCard);
     gameBoard.appendChild(newCard);
   }
 }
 
+var isTwoCards = function()
+{
+  cardsInPlay.push(this.getAttribute("data-card"));
+  console.log(this.getAttribute('data-card'));
+
+  if (this.getAttribute('data-card') === 'king') {
+    this.innerHTML = "<img src='assets/king.jpg'>";
+  } else {
+    this.innerHTML = "<img src='assets/queen.jpg'>";
+  }
+
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+
+    cardsInPlay = [];
+  }
+}
+
+var isMatch = function(cards) 
+{ 
+  // check if two cards are a match
+  if (cards[0] === cards[1]) {
+    alert("Cards are a match!");
+  } else {
+    alert("Cards do not match!");
+  }
+  // resetBoard();
+}
+
+// var board = document.getElementsByClassName('board')
+
+// var resetBoard = function()
+// {
+//   for (var i=0; i < board.length; i++)
+//   {
+//     board[i].innerHTML = ""
+//   }
+// }
+
 createBoard();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
